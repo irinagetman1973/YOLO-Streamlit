58a2f2ca655ec23503b5b46bd4bb7b025bcee801
AtomicIrina <atomichabitsforlife@gmail.com>
2023-10-06 16:34:51 +1300

    avatars stored to firebase and rendered into app successfully

--- avatar_manager.py
+++ avatar_manager.py
@@ -1,51 +1,60 @@
 # avatar_manager.py
 
 import streamlit as st
-import firebase_admin
 from firebase_admin import db, storage
 
-def upload_and_store_avatar(user_id):
-    uploaded_file = st.sidebar.file_uploader("Choose an avatar image...", type=["jpg", "png"])
-    
-    if uploaded_file is not None:
-        # You may want to use a library or service like Firebase Storage to store the image
-        # Assume store_avatar returns the URL where the avatar is stored
-        avatar_url = store_avatar(user_id, uploaded_file)
-        
-        # Now store the avatar_url in the Firebase Realtime Database
-        ref = db.reference(f'/users/{user_id}/avatar')
-        ref.set(avatar_url)
+
+
+
         
 def store_avatar(user_id, file):
-    # Assume you have already initialized Firebase Admin SDK as shown in previous examples
-
-    # Get a reference to the Firebase Storage service
-    bucket = storage.bucket()
-
+    
+      bucket = storage.bucket('capstone-c23c5.appspot.com') 
+    
     # Determine the file extension based on the mime type
-    mime_type = file.type  # assuming file.type returns the mime type
-    extension = ""
-    if mime_type == "image/png":
+      mime_type = file.type  # assuming file.type returns the mime type
+      extension = ""
+      if mime_type == "image/png":
         extension = ".png"
-    elif mime_type == "image/jpeg":
+      elif mime_type == "image/jpeg":
         extension = ".jpg"
 
-    if not extension:
+      if not extension:
         raise ValueError("Unsupported file type")
 
     # Create a unique file name based on user_id and file extension
-    blob = bucket.blob(f'avatars/{user_id}{extension}')
+      blob = bucket.blob(f'avatars/{user_id}{extension}')
+      
+      # Reset the file object's position to the beginning of the stream
+      file.seek(0)
 
     # Upload the file
-    blob.upload_from_string(file.read(), content_type=mime_type)
+      blob.upload_from_file(file, content_type=mime_type)
 
-    # Make the file publicly accessible
-    blob.make_public()
+    
+      # Make the file publicly accessible
+      blob.make_public()
+      avatar_url = blob.public_url
+
+      # Update the user's avatar URL in the Firebase Realtime Database
+      ref = db.reference(f'/users/{user_id}/avatar')
+      ref.set(avatar_url)
 
     # Return the public URL to the file
-    return blob.public_url
+      return avatar_url
 
 
-def get_avatar_url(user_id):
-    ref = db.reference(f'/users/{user_id}/avatar')
-    return ref.get()
+def get_avatar_url(user_id, avatar_url=None):
+      ref = db.reference(f'/users/{user_id}/avatar')
+      if avatar_url is not None:
+        # If avatar_url is provided, update it in the database
+        try:
+            ref.set(avatar_url)
+        except Exception as e:
+            st.error(f"Failed to update avatar URL in Firebase: {e}")
+      else:
+        # If avatar_url is not provided, get it from the database
+        try:
+            return ref.get()
+        except Exception as e:
+            st.error(f"Failed to get avatar URL from Firebase: {e}")
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import streamlit as st
 from au import authenticate
 from app import  display_main_page
-from avatar_manager import upload_and_store_avatar, get_avatar_url
+from avatar_manager import  get_avatar_url, store_avatar
 import time
+from PIL import Image, ImageDraw, ImageOps
+import requests
+from io import BytesIO
+
+#-------------Page Configuration-------------------
 st.set_page_config(
     page_title="Vehicle Tracking with YOLOv8",
     page_icon="🎓",
     layout="wide",
     initial_sidebar_state="expanded"
 )
+
+
+
 
 def display_sidebar():
     if not st.session_state.get('authenticated', False):
@@ -20,8 +28,41 @@ def display_sidebar():
         user_id = st.session_state.user["uid"]
         avatar_url = get_avatar_url(user_id)
         st.sidebar.write(f'Welcome, {st.session_state.user["username"]}')
+
+        # Check if the user has an avatar
         if avatar_url:
-            st.sidebar.image(avatar_url, caption=f"Hello, {st.session_state.username}!", width=150)
+            # Avatar exists, display it
+            response = requests.get(avatar_url)
+            
+            avatar = Image.open(BytesIO(response.content))
+        else:
+            # Avatar does not exist, offer file uploader
+            uploaded_file = st.file_uploader("Choose an avatar:", type=["jpg", "png", "jpeg"])
+            if uploaded_file:
+                avatar = Image.open(uploaded_file)
+                store_avatar(user_id, uploaded_file)  # Assume store_avatar() saves the avatar and updates the URL in the database
+                st.session_state['avatar_url'] = get_avatar_url(user_id)  # Update the session_state with the new avatar URL
+            else:
+                return  # Return early if no file is uploaded, to avoid running the code below with a None avatar
+
+        #-------avatar displaying-------------------
+        # avatar = Image.open("avatars/fashion-little-boy.jpg")
+
+        # Create a mask of a filled circle
+        mask = Image.new("L", avatar.size)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0) + avatar.size, fill=255)
+
+        # Apply the mask to the avatar
+        rounded_avatar = Image.new("RGBA", avatar.size)
+        rounded_avatar.paste(avatar, mask=mask)
+        avatar_width = 100  
+        # Display the rounded avatar in Streamlit
+        st.sidebar.image(rounded_avatar,  width=avatar_width)
+
+
+        # if avatar_url:
+        #     st.sidebar.image(avatar_url, width=150)
             #--------Logout button-------------------
         if st.sidebar.button("Logout", key="logout_button"):
             st.session_state.pop('user', None)  # Remove user info from session_state
