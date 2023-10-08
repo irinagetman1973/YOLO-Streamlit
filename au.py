6e4aaa6395479402ce61808fb27e1fc1d81705f2
AtomicIrina <atomichabitsforlife@gmail.com>
2023-10-08 14:43:17 +1300

    installed plotly for image inference

--- au.py
+++ au.py
@@ -32,6 +32,8 @@ firebaseConfig = config_data['firebaseConfig']
 # cred = credentials.Certificate('C:\\Users\\irina\\capstone\\capstone-c23c5-4e7a43be2c53.json')
 
 cred_path = os.environ.get('FIREBASE_CERT_PATH')
+# st.write(type(cred_path))
+
 cred_path = cred_path.strip('"')  # This line will remove any double quotes from the beginning and end of cred_path
 
 db_url = os.environ.get('FIREBASE_DB_URL')
