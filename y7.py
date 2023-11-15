009510ae78424e96803869a6426ab951879581b6
Irina Getman <irinagetman1973@gmail.com>
2023-11-16 09:54:47 +1300

    Committing changes to y7.py before removal

--- y7.py
+++ y7.py
@@ -6,8 +6,20 @@ from yolov7.yolov7_wrapper import YOLOv7Wrapper
 from PIL import Image
 import numpy as np
 from io import BytesIO
-import tempfile
-import cv2
+import time
+import psutil
+
+import psutil
+import time
+from PIL import Image
+
+import io
+import numpy as np
+import streamlit as st
+
+
+
+
 
 
 
@@ -16,7 +28,7 @@ def main():
 
     # 1. Model Selection:
     model_names = list(YOLOv7Wrapper.MODELS.keys())
-    selected_model = st.selectbox('Choose a YOLOv7 Model:', model_names)
+    selected_model = st.selectbox('Choose a YOLOv7 Model:', model_names, key="model_selectbox_v7")
 
     uploaded_file = st.file_uploader("Choose an image...", type="jpg")
     confidence_threshold = st.slider('Confidence Threshold', min_value=0.0, max_value=1.0, value=0.5, step=0.01)
@@ -28,93 +40,58 @@ def main():
         im0 = np.array(im0)
         st.image(im0, caption='Uploaded Image.', use_column_width=True)
 
+        # Redirect stdout
+        original_stdout = sys.stdout
+        sys.stdout = new_stdout = io.StringIO()
+
         # Initialize the wrapper
         wrapper = YOLOv7Wrapper(model_name=selected_model)
+
+        # Reset stdout to its original form
+        sys.stdout = original_stdout
+
+        # Get the output and print it to Streamlit
+        output = new_stdout.getvalue()
+        if output:
+            st.text("Model logs:")
+            st.text(output)
+
+        # Record the initial CPU percentage
+        initial_cpu = psutil.cpu_percent(interval=None)
+
+        # --- Start time --------
+        start_time = time.time()
 
         # Detect and draw the boxes
         detections_img, captions = wrapper.detect_and_draw_boxes_from_np(im0, confidence_threshold)
 
+        # ------- END TIME -------
+        end_time = time.time()
+        inference_time = end_time - start_time
+
+        # Calculate the CPU usage during inference
+        final_cpu = psutil.cpu_percent(interval=None)
+        cpu_usage_during_inference = final_cpu - initial_cpu
+
+        st.markdown(f"Inference time: {inference_time:.4f} seconds")
+        st.markdown(f"CPU usage during inference: {cpu_usage_during_inference:.2f}%")
+
         # Display the detections' captions if available
-        if captions:
+        if len(captions) > 0:
             st.write("\n".join(captions))
+            num_boxes = len(captions)
+            st.write(f"Number of detections: {num_boxes}")
+        else:
+            st.write("No detections found!")
 
         # Display the results
         st.image(detections_img, caption='Processed Image with Detections', use_column_width=True)
 
 
 
-# def main():
-#     st.title("YOLOv7 Detection Test")
 
-#     uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-    
-#     if uploaded_file is not None:
-#         col1, col2 = st.columns(2)
 
-#         with col1:
-#             st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
-#             st.markdown("**Original Image**")
 
-#         # Initialize the wrapper
-#         wrapper = YOLOv7Wrapper(model_name="yolov7-e6.pt")
-
-#         if st.button("Detect Objects"):
-#             with st.spinner("Running detection..."):
-#                 image = Image.open(uploaded_file)
-#                 image_np = np.array(image)
-#                 detections_img, captions = wrapper.detect_and_draw_boxes_from_np(image_np)
-
-#             with col2:
-#                 if detections_img is not None:
-#                     st.image(detections_img, caption='Processed Image with Detections', use_column_width=True)
-#                     st.markdown("**Detected Image**")
-
-#                     detection_results = ""
-#                     count_dict = {}
-
-#                     for caption in captions:
-#                         object_type, coordinates, score = process_caption(caption)  # Assuming you have a method to process captions
-#                         detection_results += f"<b style='color: blue;'>Object type:</b> {object_type}<br><b style='color: blue;'>Coordinates:</b> {coordinates}<br><b style='color: blue;'>Probability:</b> {score}<br>---<br>"
-#                         if object_type in count_dict:
-#                             count_dict[object_type] += 1
-#                         else:
-#                             count_dict[object_type] = 1
-
-#                     for object_type, count in count_dict.items():
-#                         detection_results += f"<b style='color: blue;'>Count of {object_type}:</b> {count}<br>"
-
-#                     scrollable_textbox = f"""
-#                         <div style="
-#                             font-family: 'Source Code Pro','monospace';
-#                             font-size: 16px;
-#                             overflow-y: scroll;
-#                             border: 1px solid #000;
-#                             padding: 10px;
-#                             width: 500px;
-#                             height: 400px;
-#                         ">
-#                             {detection_results}
-#                         </div>
-#                     """
-#                     st.markdown("""### Results:""")
-#                     st.markdown(scrollable_textbox, unsafe_allow_html=True)
-
-#                 else:
-#                     st.markdown("""### No objects detected""")
-#                     st.markdown("""
-#                         The model did not detect any objects in the uploaded image.
-#                         Please try with a different image or adjust the model's 
-#                         confidence threshold in the sidebar and try again.
-#                     """)
-
-# def process_caption(caption):
-#     # Assuming captions are in format "object_type (coordinates): score"
-#     # Example: "car (23,45,67,89): 0.85"
-#     object_type, rest = caption.split(" ", 1)
-#     coordinates, score = rest.split(":")
-#     score = float(score.strip())
-#     coordinates = coordinates.strip()[1:-1]  # Removing parentheses
-#     return object_type, coordinates, score
 
 
 
